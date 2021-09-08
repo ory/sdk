dff44d8db9eee21043709b1e16172ee1b1137784
aeneasr <3372410+aeneasr@users.noreply.github.com>
2021-09-08 19:22:00 +0300

    fix: resolve dotnet issue

--- scripts/test.sh
+++ scripts/test.sh
@@ -36,42 +36,42 @@ php() {
 }
 
 python () {
-  echo "Generating Python..."
+  echo "Testing Python..."
 
   dir="clients/${PROJECT}/python"
   (cd "$dir"; pip install -r requirements.txt; pip install -r test-requirements.txt; pytest --cov="$PYTHON_PACKAGE_NAME")
 }
 
 ruby () {
-  echo "Generating Ruby..."
+  echo "Testing Ruby..."
 
   dir="clients/${PROJECT}/ruby"
   (cd "$dir"; rm "${RUBY_PROJECT_NAME}-${GEM_VERSION}.gem" || true; bundle install --path vendor/bundle; bundle exec rspec; gem build "${RUBY_PROJECT_NAME}.gemspec"; gem install "${RUBY_PROJECT_NAME}-${GEM_VERSION}.gem")
 }
 
 golang () {
-  echo "Generating Golang..."
+  echo "Testing Golang..."
 
   dir="clients/${PROJECT}/go"
   (cd "${dir}"; go mod tidy; go build -o "$(mktemp)" .)
 }
 
 dotnet () {
-  echo "Generating dotnet..."
+  echo "Testing dotnet..."
 
   dir="clients/${PROJECT}/dotnet"
   (cd "$dir"; dotnet build -c Release; dotnet test -c Release)
 }
 
 dart () {
-  echo "Generating Dart..."
+  echo "Testing Dart..."
 
   dir="clients/${PROJECT}/dart"
   (cd "$dir"; dart test .)
 }
 
 rust () {
-  echo "Generating Rust..."
+  echo "Testing Rust..."
 
   dir="clients/${PROJECT}/rust"
   (cd "$dir"; cargo test)
