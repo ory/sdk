3ca2571137c221a47ae68b818948aa2203e8638c
aeneasr <3372410+aeneasr@users.noreply.github.com>
2021-09-08 19:28:55 +0300

    fix: replace ; with &&

--- scripts/test.sh
+++ scripts/test.sh
@@ -18,63 +18,63 @@ typescript () {
   echo "Testing TypeScript..."
 
   dir="clients/${PROJECT}/typescript"
-  (cd "$dir"; npm i; npm run build)
+  (cd "$dir" && npm i && npm run build)
 }
 
 java () {
   echo "Testing Java..."
 
   dir="clients/${PROJECT}/java"
-  (cd "$dir"; mvn test-compile)
+  (cd "$dir" && mvn test-compile)
 }
 
 php() {
   echo "Testing PHP..."
 
   dir="clients/${PROJECT}/php"
-  (cd "$dir"; composer install; ./vendor/bin/phpunit)
+  (cd "$dir" && composer install && ./vendor/bin/phpunit)
 }
 
 python () {
   echo "Testing Python..."
 
   dir="clients/${PROJECT}/python"
-  (cd "$dir"; pip install -r requirements.txt; pip install -r test-requirements.txt; pytest --cov="$PYTHON_PACKAGE_NAME")
+  (cd "$dir" && pip install -r requirements.txt && pip install -r test-requirements.txt && pytest --cov="$PYTHON_PACKAGE_NAME")
 }
 
 ruby () {
   echo "Testing Ruby..."
 
   dir="clients/${PROJECT}/ruby"
-  (cd "$dir"; rm "${RUBY_PROJECT_NAME}-${GEM_VERSION}.gem" || true; bundle install --path vendor/bundle; bundle exec rspec; gem build "${RUBY_PROJECT_NAME}.gemspec"; gem install "${RUBY_PROJECT_NAME}-${GEM_VERSION}.gem")
+  (cd "$dir" && rm "${RUBY_PROJECT_NAME}-${GEM_VERSION}.gem" || true && bundle install --path vendor/bundle && bundle exec rspec && gem build "${RUBY_PROJECT_NAME}.gemspec" && gem install "${RUBY_PROJECT_NAME}-${GEM_VERSION}.gem")
 }
 
 golang () {
   echo "Testing Golang..."
 
   dir="clients/${PROJECT}/go"
-  (cd "${dir}"; go mod tidy; go build -o "$(mktemp)" .)
+  (cd "${dir}" && go mod tidy && go build -o "$(mktemp)" .)
 }
 
 dotnet () {
   echo "Testing dotnet..."
 
   dir="clients/${PROJECT}/dotnet"
-  (cd "$dir"; dotnet build -c Release; dotnet test -c Release)
+  (cd "$dir" && dotnet build -c Release && dotnet test -c Release)
 }
 
 dart () {
   echo "Testing Dart..."
 
   dir="clients/${PROJECT}/dart"
-  (cd "$dir"; dart test .)
+  (cd "$dir" && dart test .)
 }
 
 rust () {
   echo "Testing Rust..."
 
   dir="clients/${PROJECT}/rust"
-  (cd "$dir"; cargo test)
+  (cd "$dir" && cargo test)
 }
 
 typescript
