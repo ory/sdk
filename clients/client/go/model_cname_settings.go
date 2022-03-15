671b8b90c344f6a8157cb3b5b5d675985e634349
aeneasr <3372410+aeneasr@users.noreply.github.com>
2022-03-15 10:40:38 +0000

    chore: regenerate OpenAPI client v0.0.1-alpha.129

--- clients/client/go/model_cname_settings.go
+++ clients/client/go/model_cname_settings.go
@@ -3,7 +3,7 @@
  *
  * Documentation for all public and administrative Ory APIs. Administrative APIs can only be accessed with a valid Personal Access Token. Public APIs are mostly used in browsers. 
  *
- * API version: v0.0.1-alpha.128
+ * API version: v0.0.1-alpha.129
  * Contact: support@ory.sh
  */
 
