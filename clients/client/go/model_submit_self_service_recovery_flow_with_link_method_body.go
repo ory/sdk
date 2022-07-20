19bb3e7697c39c55157778aa7e5602d98b2e5824
aeneasr <3372410+aeneasr@users.noreply.github.com>
2022-07-20 14:27:50 +0000

    chore: regenerate OpenAPI client v0.1.0-alpha.7

--- clients/client/go/model_submit_self_service_recovery_flow_with_link_method_body.go
+++ clients/client/go/model_submit_self_service_recovery_flow_with_link_method_body.go
@@ -3,7 +3,7 @@
  *
  * Documentation for all public and administrative Ory APIs. Administrative APIs can only be accessed with a valid Personal Access Token. Public APIs are mostly used in browsers. 
  *
- * API version: v0.1.0-alpha.6
+ * API version: v0.1.0-alpha.7
  * Contact: support@ory.sh
  */
 
