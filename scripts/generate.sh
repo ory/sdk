3dca508f46ae213ac61c3f5d25a3c8cac2c12fb9
Dennis Kugelmann <dennis.kugelmann@simpleclub.com>
2023-05-24 15:19:41 +0200

    docs(dart): add link to source code / repository for pub.dev (#270)

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -251,7 +251,7 @@ dart () {
   rm -rf "$dir" || true
   mkdir -p "$dir"
 
-  openapi-generator-cli version-manager set 6.1.0
+  openapi-generator-cli version-manager set 6.6.0
   openapi-generator-cli generate -i "${SPEC_FILE}" \
     -g dart-dio \
     -o "$dir" \
