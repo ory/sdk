395e12f910817fa330f43477f9c5ef69097db73e
aeneasr <3372410+aeneasr@users.noreply.github.com>
2023-09-08 11:29:21 +0000

    chore: regenerate OpenAPI client v1.2.3

--- clients/client/elixir/lib/ory/connection.ex
+++ clients/client/elixir/lib/ory/connection.ex
@@ -164,7 +164,7 @@ defmodule Ory.Connection do
         Keyword.get(
           tesla_options,
           :user_agent,
-          "openapi-generator - Ory v1.2.2 - elixir"
+          "openapi-generator - Ory v1.2.3 - elixir"
         )
       )
 
