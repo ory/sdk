1132bc87ecb64d5e98cd09d079f55a96ad4d0268
aeneasr <3372410+aeneasr@users.noreply.github.com>
2025-02-24 23:47:53 +0100

    fix: dart publishing

---
+++ contrib/dart/CHANGELOG.md
@@ -0,0 +1,1 @@
+Please refer to https://changelog.ory.sh and https://www.ory.sh/docs/sdk
--- scripts/generate.sh
+++ scripts/generate.sh
@@ -244,7 +244,7 @@ dart () {
   rm -rf "$dir" || true
   mkdir -p "$dir"
 
-  openapi-generator-cli version-manager set 7.7.0
+  openapi-generator-cli version-manager set 7.11.0
   openapi-generator-cli generate -i "${SPEC_FILE}" \
     -g dart-dio \
     -o "$dir" \
