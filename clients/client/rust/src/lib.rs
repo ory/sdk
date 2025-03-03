d538d5b744dcb05af1cceac1449159b81bef39dd
Alwin Meijboom <alwin@brainhive.nl>
2025-03-03 16:08:51 +0100

    feat: add feature flag to support rustls-tls for rust client

--- clients/client/rust/src/lib.rs
+++ clients/client/rust/src/lib.rs
@@ -4,7 +4,10 @@
 extern crate serde;
 extern crate serde_json;
 extern crate url;
+#[cfg(feature = "default")]
 extern crate reqwest;
+#[cfg(feature = "rustls")]
+extern crate reqwest_rustls as reqwest;
 
 pub mod apis;
 pub mod models;
