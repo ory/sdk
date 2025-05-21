97e2d9c11ba6f061480d46b52018884d7a8869e9
Tyler Hawkes <tylerhawkes@users.noreply.github.com>
2025-05-21 04:46:47 -0600

    Use 7.13 to generate rust client (#427)
    openapi generator 7.13 disables reqwest default features which turns off the openssl dependency.

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -269,7 +269,7 @@ rust () {
   rm -rf "$dir" || true
   mkdir -p "$dir"
 
-  npx @openapitools/openapi-generator-cli@2.17.0 version-manager set 7.12.0
+  npx @openapitools/openapi-generator-cli@2.17.0 version-manager set 7.13.0
   npx @openapitools/openapi-generator-cli@2.17.0 generate -i "${SPEC_FILE}" \
     -g rust \
     -o "$dir" \
